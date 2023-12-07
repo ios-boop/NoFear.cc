315054e626da4d0b45bb0d3583e8dcec8524199c
ios-boop <133600140+ios-boop@users.noreply.github.com>
2023-12-07 00:27:54 +0000

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,19 @@
+local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
+local window = DrRayLibrary:Load("DrRay", "Default")
+local tab = DrRayLibrary.newTab("My Tab", "ImageIdHere")
+
+tab.newToggle("Toggle", "Toggle! (prints the state)", true, function(toggleState)
+    if toggleState then
+        print("On")
+    else
+        print("Off")
+    end
+end)
+
+tab.newToggle("Toggle", "Toggle! (prints the state)", true, function(toggleState)
+    if toggleState then
+        print("On")
+    else
+        print("Off")
+    end
+end)
